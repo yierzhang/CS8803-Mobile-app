ab9664a3b63bd28dc700df9abc3cc7dbdeafe45c
Yier Zhang <yier_0227@gatech.edu>
2019-09-03 23:30:54 -0400

    add styling to landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+const titleLarge = {
+    color: "blue",
+    'text-align': "center",
+    'font-size': "60px",
+    fontFamily: "Comic Sans MS",
+};
+
+const titleSmall = {
+    'text-align': "center",
+    'font-size': "30px",
+    fontFamily: "Comic Sans MS",
+};
 const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-  </div>
+    <div>
+        <p style={titleLarge}>Welcome to our simple To-do list app!</p>
+        <p style={titleSmall}>Take some time to register and discover the feature :)</p>
+    </div>
 );
 export default Landing;
